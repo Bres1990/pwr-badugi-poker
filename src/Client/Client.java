5fb83041916fb8138735de7b55f5182558c4fc71
Bres1990 <adam.poteralowicz@gmail.com>
2014-11-20 00:57:53 +0100

    Initialized the project

---
+++ src/Client/Client.java
@@ -0,0 +1,20 @@
+package Client;
+
+import java.awt.EventQueue;
+/**
+ * Created by Adam on 2014-11-20.
+ */
+public class Client
+{
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                
+            }
+        });
+    }
+}
